06ae15195e1dd392a2975e2321649610a5271438
BlagoDamqnov <chessplayer89@abv.bg>
2022-10-24 12:06:27 +0300

    Revert "Create DB Models"
    This reverts commit e4c83d68143e106253bd78641a3d19b7e224edb2.

--- VividTracker.Data/Migrations/00000000000000_CreateIdentitySchema.Designer.cs
+++ VividTracker.Data/Migrations/00000000000000_CreateIdentitySchema.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using System;
 using VividTracker.Data;
@@ -9,9 +10,10 @@ using VividTracker.Data;
 namespace VividTracker.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("00000000000000_CreateIdentitySchema")]
+    partial class CreateIdentitySchema
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- VividTracker.Data/Models/ApplicationUser.cs
+++ VividTracker.Data/Models/ApplicationUser.cs
@@ -4,7 +4,5 @@ namespace VividTracker.Data.Models
 {
     public class ApplicationUser : IdentityUser
     {
-        public int TenantId { get;set; }
-        public virtual Tenant Tenant { get;set; }
     }
 }
