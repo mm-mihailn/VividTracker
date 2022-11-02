bc3925d00f6c7cdbeb0714466f075943f7bdec2c
GoofyGalaxy374 <prophetyordanov@gmail.com>
2022-11-02 16:53:46 +0200

    The tenants page is no longer visible to the non-registered users

--- VividTracker/ClientApp/src/App.js
+++ VividTracker/ClientApp/src/App.js
@@ -20,7 +20,7 @@ export default class App extends Component {
         <Route exact path='/' component={Home} />
         <Route path='/counter' component={Counter} />
         
-        <Route path='/tenants' component={TenantsComponent} />
+        <AuthorizeRoute path='/tenants' component={TenantsComponent} />
 
         <AuthorizeRoute path='/fetch-data' component={FetchData} />
         <Route path={ApplicationPaths.ApiAuthorizationPrefix} component={ApiAuthorizationRoutes} />
