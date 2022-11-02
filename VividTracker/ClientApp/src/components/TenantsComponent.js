0714b687d400e81c38f6f8d2b4b085ec2b43ce12
GoofyGalaxy374 <prophetyordanov@gmail.com>
2022-11-02 16:47:52 +0200

    Tenants are now successfully fetched from the database

--- VividTracker/ClientApp/src/components/TenantsComponent.js
+++ VividTracker/ClientApp/src/components/TenantsComponent.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react'
 
 export default class TenantsComponent extends Component {
+
+    constructor()
+    {
+        super()
+        this.state = {tenants: []}
+    }
+
+    componentDidMount()
+    {
+        fetch('https://localhost:7091/api/getAllTenants')
+        .then((res) => res.json())
+        .then((res) => this.setState({tenants: res}))
+    
+    }
   render() {
     return (
       <div>TenantsComponent</div>
