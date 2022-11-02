e5f4c833f27d661d064bb3f27e78ee6c01f0d0a0
GoofyGalaxy374 <prophetyordanov@gmail.com>
2022-11-02 16:41:37 +0200

    Added tenants route to the react-router

--- VividTracker/ClientApp/src/App.js
+++ VividTracker/ClientApp/src/App.js
@@ -9,6 +9,7 @@ import ApiAuthorizationRoutes from './components/api-authorization/ApiAuthorizat
 import { ApplicationPaths } from './components/api-authorization/ApiAuthorizationConstants';
 
 import './custom.css'
+import TenantsComponent from './components/TenantsComponent';
 
 export default class App extends Component {
   static displayName = App.name;
@@ -18,6 +19,9 @@ export default class App extends Component {
       <Layout>
         <Route exact path='/' component={Home} />
         <Route path='/counter' component={Counter} />
+        
+        <Route path='/tenants' component={TenantsComponent} />
+
         <AuthorizeRoute path='/fetch-data' component={FetchData} />
         <Route path={ApplicationPaths.ApiAuthorizationPrefix} component={ApiAuthorizationRoutes} />
       </Layout>
