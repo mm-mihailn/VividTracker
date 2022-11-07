582cf0ec636534e21fb5f8897491c3f588c43734
Mihail Nikolov <mihail.nikolov@mentormate.com>
2022-11-07 15:38:22 +0200

    Added layered implementation - Skeleton for Repositories and Services

---
+++ VividTracker.Business/Services/Interfaces/ITenantsService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VividTracker.Data.Models;
+
+namespace VividTracker.Business.Services.Interfaces
+{
+    public interface ITenantsService
+    {
+        Task<IEnumerable<Tenant>> GetTenantsAsync();
+        Task<Tenant?> GetTenantByIdAsync(int id);
+    }
+}
---
+++ VividTracker.Business/Services/TenantsService.cs
@@ -0,0 +1,20 @@
+﻿using VividTracker.Business.Services.Interfaces;
+using VividTracker.Data.Models;
+using VividTracker.Data.Repositories.Interfaces;
+
+namespace VividTracker.Business.Services;
+
+public class TenantsService : ITenantsService
+{
+    private readonly ITenantsRepository _tenantsRepository;
+
+    public TenantsService(ITenantsRepository tenantsRepository)
+    {
+        _tenantsRepository = tenantsRepository;
+    }
+
+    public async Task<IEnumerable<Tenant>> GetTenantsAsync() => await _tenantsRepository.GetAllTenants();
+
+    public async Task<Tenant?> GetTenantByIdAsync(int id) => await _tenantsRepository.FindAsync(id);
+    
+}
