0749542dd078574e53edfc9d34a4362bb4ad2217
blueonn <marti5681433@gmail.com>
2022-11-09 17:51:02 +0200

    Added the matrix as a component

---
+++ VividTracker/ClientApp/src/components/ColorMatrix.js
@@ -0,0 +1,20 @@
+﻿import React, { Component } from 'react';
+import './ColorMatrix.css';
+import './ColorMatrixJS.js';
+
+export class ColorMatrix extends Component {
+    static displayName = ColorMatrix.name;
+
+    render() {
+        return (
+            <div className='colorMatrix'>
+
+                    <div class="firstColumn">
+
+                        <p id="square1" class="square"></p>
+
+                    </div>
+            </div>
+        );
+    }
+}
---
+++ VividTracker/ClientApp/src/components/ColorMatrixJS.js
@@ -0,0 +1,60 @@
+﻿import $ from 'jquery'; 
+
+setInterval(ChangeColor, 300);
+
+function ChangeColor() {
+    var index = Math.floor(Math.random() * 8);
+    var newIndex = Math.floor(Math.random() * 161);
+    var newIndex1 = Math.floor(Math.random() * 161);
+    var newIndex2 = Math.floor(Math.random() * 161);
+    var squareColor = "";
+
+    switch (index) {
+        case 0:
+            squareColor = '#D9D9D9'
+            break;
+        case 1:
+            squareColor = '#F03C31'
+            break;
+        case 2:
+            squareColor = '#EEDC3A'
+            break;
+        case 3:
+            squareColor = '#51FF4E'
+            break;
+        case 4:
+            squareColor = '#0239FB'
+            break;
+        case 5:
+            squareColor = '#708FFF'
+            break;
+        case 6:
+            squareColor = '#D2DCFF'
+            break;
+        case 7:
+            squareColor = '#9CB2FF'
+            break;
+    }
+
+
+    function ColorSquares() {
+        var value = document.getElementsByClassName("square")[newIndex];
+        var value1 = document.getElementsByClassName("square")[newIndex1];
+        var value2 = document.getElementsByClassName("square")[newIndex2];
+
+        value.style.background = squareColor;
+        value1.style.background = squareColor;
+        value2.style.background = squareColor;
+    }
+
+    ColorSquares();
+}
+$(document).ready(function () {
+    $("#square1").hide();
+    $("#square1").show();
+    for (var i = 0; i < 160; i++) {
+        $("#square1").append(`<p class="square"></p>`);
+    }
+    $("#btn-square").hide();
+    $(".firstColumn").css("column-count", "16");
+});
