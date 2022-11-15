3ef363cc8b284b5a536950f7610003ef3b34b9ff
Mihail Nikolov <mihail.nikolov@mentormate.com>
2022-11-15 23:20:52 +0200

    Removed the old implementation files and refactored the code

--- VividTracker/ClientApp/src/components/ColorMatrix/ColorMatrix.js
+++ VividTracker/ClientApp/src/components/ColorMatrix/ColorMatrix.js
@@ -3,77 +3,68 @@ import './ColorMatrix.css';
 import $ from 'jquery'; 
 
 export class ColorMatrix extends Component {
-    static displayName = ColorMatrix.name;
-
-    ChangeColor() {
-        
-        var colorIndex = Math.floor(Math.random() * 8);
-        var firstSquareIndex = Math.floor(Math.random() * 161);
-        var secondSquareIndex = Math.floor(Math.random() * 161);
-        var thirdSquareIndex = Math.floor(Math.random() * 161);
+    changeColor() {
+        const colorIndex = Math.floor(Math.random() * 8);
+        const firstSquareIndex = Math.floor(Math.random() * 161);
+        const secondSquareIndex = Math.floor(Math.random() * 161);
+        const thirdSquareIndex = Math.floor(Math.random() * 161);
         let squareColor = '#000000';
 
         switch (colorIndex) {
             case 0:
-                squareColor = '#D9D9D9'
+                squareColor = '#D9D9D9';
                 break;
             case 1:
-                squareColor = '#F03C31'
+                squareColor = '#F03C31';
                 break;
             case 2:
-                squareColor = '#EEDC3A'
+                squareColor = '#EEDC3A';
                 break;
             case 3:
-                squareColor = '#51FF4E'
+                squareColor = '#51FF4E';
                 break;
             case 4:
-                squareColor = '#0239FB'
+                squareColor = '#0239FB';
                 break;
             case 5:
-                squareColor = '#708FFF'
+                squareColor = '#708FFF';
                 break;
             case 6:
-                squareColor = '#D2DCFF'
+                squareColor = '#D2DCFF';
                 break;
             case 7:
-                squareColor = '#9CB2FF'
+                squareColor = '#9CB2FF';
                 break;
         }
 
-        function ColorSquares(color) {
+        let firstSquare = document.getElementsByClassName("squares")[firstSquareIndex];
+        let secondSquare = document.getElementsByClassName("squares")[secondSquareIndex];
+        let thirdSquare = document.getElementsByClassName("squares")[thirdSquareIndex];
 
-            let firstSquare = document.getElementsByClassName("squares")[firstSquareIndex];
-            let secondSquare = document.getElementsByClassName("squares")[secondSquareIndex];
-            let thirdSquare = document.getElementsByClassName("squares")[thirdSquareIndex];
-
-            if (firstSquare !== undefined && secondSquare !== undefined && thirdSquare !== undefined) {
-                firstSquare.style.backgroundColor = color;
-                secondSquare.style.backgroundColor = color;
-                thirdSquare.style.backgroundColor = color;
-            }
+        if (firstSquare !== undefined && secondSquare !== undefined && thirdSquare !== undefined) {
+            firstSquare.style.backgroundColor = squareColor;
+            secondSquare.style.backgroundColor = squareColor;
+            thirdSquare.style.backgroundColor = squareColor;
         }
-
-        ColorSquares(squareColor);
     }
 
-    CreateMatrix = () => {
-        $(document).ready(function () {
+    createMatrix = () => {
 
-            for (var i = 0; i < 160; i++) {
+
+            for (let i = 0; i < 160; i++) {
                 $(".matrix").append(`<p class="squares" ></p>`);
             }
 
             $(".matrix").css("column-count", "16");
-        });
+
     }
 
     componentDidMount() {
-        this.CreateMatrix()
+        this.createMatrix()
     }
 
     render() {
-
-        setInterval(this.ChangeColor, 300)
+        setInterval(this.changeColor, 300)
         return (
 
             <div className="colorMatrix">
@@ -85,5 +76,4 @@ export class ColorMatrix extends Component {
 
         );
     }
-
 }
