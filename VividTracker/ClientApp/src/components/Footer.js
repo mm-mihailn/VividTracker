900ec8156cdc4ecff5cecf8b742956d1418a507c
BlagoDamqnov <chessplayer89@abv.bg>
2022-11-16 21:30:01 +0200

    Created footer

---
+++ VividTracker/ClientApp/src/components/Footer.js
@@ -0,0 +1,22 @@
+﻿import React, { Component } from 'react';
+import '../custom.css'
+
+export class Footer extends Component {
+    static displayName = Footer.name;
+
+    render() {
+        return (
+            <footer id='footer'>
+                <div id="team-info">
+                    <ul>
+                        <li>Blagovest Damyanov</li>
+                        <li>Martin Yordanov</li>
+                        <li>Maetin Marinov</li>
+                        <li>Veli Ashikov</li>
+                        <li>Mihail Nikolov</li>
+                    </ul>
+                </div>
+            </footer>
+        );
+    }
+}
