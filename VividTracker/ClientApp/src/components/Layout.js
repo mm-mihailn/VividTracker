f9ef039c733a3635b9b0443350123b219078859a
BlagoDamqnov <chessplayer89@abv.bg>
2022-11-21 16:27:29 +0200

    Update backgroudn color and remove container element

--- VividTracker/ClientApp/src/components/Layout.js
+++ VividTracker/ClientApp/src/components/Layout.js
@@ -10,9 +10,9 @@ export class Layout extends Component {
     return (
       <div>
         <NavMenu />
-        <Container>
+       {/* <Container>*/}
           {this.props.children}
-            </Container>
+         {/*   </Container>*/}
          <Footer/>
       </div>
     );
