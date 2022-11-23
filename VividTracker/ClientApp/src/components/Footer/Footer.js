b3946763cd207625f04a2392fcfca0cc19e039b1
BlagoDamqnov <chessplayer89@abv.bg>
2022-11-23 17:49:39 +0200

    Maked responsive footer

--- VividTracker/ClientApp/src/components/Footer/Footer.js
+++ VividTracker/ClientApp/src/components/Footer/Footer.js
@@ -8,27 +8,27 @@ export class Footer extends Component {
         return (
             <footer id='footer' className='fixed-bottom'>
                
-                <div className="row">
-                    <div className="col-xs-6 col-sm-4 col-md-4 col-lg-4 col-xl-6">
+                <div className="row justify-content-center">
+                    <div className="col-xs-6 col-sm-4 col-md-4 col-lg-4 col-xl-6 col-12 ">
                         <h5>Blagovest Damqnov</h5>
                     </div>
-                    <div className="col-xs-6 col-sm-4 col-md-4 col-lg-4 col-xl-6">
+                    <div className="col-xs-6 col-sm-4 col-md-4 col-lg-4 col-xl-6 col-12">
                         <h5>Martin Yordanov</h5>
                     </div>
                 </div>
-                <div className="row">
-                    <div className="col-xs-6 col-sm-4 col-md-4 col-lg-4 col-xl-6">
+                <div className="row justify-content-center">
+                    <div className="col-xs-6 col-sm-4 col-md-4 col-lg-4 col-xl-6 col-12">
                         <h5>Martin Marinov</h5>
                     </div>
-                    <div className="col-xs-6 col-sm-4 col-md-4 col-lg-4 col-xl-6">
+                    <div className="col-xs-6 col-sm-4 col-md-4 col-lg-4 col-xl-6 col-12">
                         <h5>Veli Aschikov</h5>
                     </div>
                 </div>
-                <div className="row">
-                    <div className="col-xs-6 col-sm-4 col-md-4 col-lg-4 col-xl-6">
+                <div className="row  justify-content-center">
+                    <div className="col-xs-6 col-sm-4 col-md-4 col-lg-4 col-xl-6 col-12">
                         <h5>Kristiqn Manov</h5>
                     </div>
-                    <div className="col-xs-6 col-sm-4 col-md-4 col-lg-4 col-xl-6">
+                    <div className="col-xs-6 col-sm-4 col-md-4 col-lg-4 col-xl-6 col-12">
                         <h5>Gabriel Yordanov</h5>
                     </div>
                 </div>
