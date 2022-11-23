718aef49305f008c87af3cef1730977c022624f2
BlagoDamqnov <chessplayer89@abv.bg>
2022-11-23 18:42:07 +0200

    Added a div and center items in the footer

--- VividTracker/ClientApp/src/components/Footer/Footer.js
+++ VividTracker/ClientApp/src/components/Footer/Footer.js
@@ -7,31 +7,33 @@ export class Footer extends Component {
     render() {
         return (
             <footer id='footer' className='fixed-bottom'>
-               
-                <div className="row justify-content-center">
-                    <div className="col-xs-6 col-sm-4 col-md-4 col-lg-4 col-xl-6 col-12 ">
-                        <h5>Blagovest Damqnov</h5>
-                    </div>
+                <div id ="main-content">
+                  <div className="row">
+                        <div className="col-xs-6 col-sm-4 col-md-4 col-lg-4 col-xl-6 col-12">
+                            <h5>Martin Yordanov</h5>
+                        </div>
                     <div className="col-xs-6 col-sm-4 col-md-4 col-lg-4 col-xl-6 col-12">
-                        <h5>Martin Yordanov</h5>
+                       <h5>Blagovest Damqnov</h5>
                     </div>
                 </div>
-                <div className="row justify-content-center">
-                    <div className="col-xs-6 col-sm-4 col-md-4 col-lg-4 col-xl-6 col-12">
-                        <h5>Martin Marinov</h5>
+                    <div className="row">
+                        <div className="col-xs-6 col-sm-4 col-md-4 col-lg-4 col-xl-6 col-12">
+                            <h5>Martin Marinov</h5>
+                        </div>
+                        <div className="col-xs-6 col-sm-4 col-md-4 col-lg-4 col-xl-6 col-12">
+                            <h5>Veli Aschikov</h5>
+                        </div>
                     </div>
-                    <div className="col-xs-6 col-sm-4 col-md-4 col-lg-4 col-xl-6 col-12">
-                        <h5>Veli Aschikov</h5>
+                    <div className="row">
+                        <div className="col-xs-6 col-sm-4 col-md-4 col-lg-4 col-xl-6 col-12">
+                            <h5>Kristiqn Manov</h5>
+                        </div>
+                        <div className="col-xs-6 col-sm-4 col-md-4 col-lg-4 col-xl-6 col-12">
+                            <h5>Gabriel Yordanov</h5>
+                        </div>
                     </div>
                 </div>
-                <div className="row  justify-content-center">
-                    <div className="col-xs-6 col-sm-4 col-md-4 col-lg-4 col-xl-6 col-12">
-                        <h5>Kristiqn Manov</h5>
-                    </div>
-                    <div className="col-xs-6 col-sm-4 col-md-4 col-lg-4 col-xl-6 col-12">
-                        <h5>Gabriel Yordanov</h5>
-                    </div>
-                </div>
+              
             </footer>
         );
     }
