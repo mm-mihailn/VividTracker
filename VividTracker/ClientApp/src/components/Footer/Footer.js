01326af60656c4a0653abb6e1ceac1345d41512e
BlagoDamqnov <chessplayer89@abv.bg>
2022-11-23 17:24:58 +0200

    Created padding top of the footer content

--- VividTracker/ClientApp/src/components/Footer/Footer.js
+++ VividTracker/ClientApp/src/components/Footer/Footer.js
@@ -9,26 +9,26 @@ export class Footer extends Component {
             <footer id='footer' className='fixed-bottom'>
                
                 <div className="row">
-                    <div className="col-xs-6 col-sm-4 col-md-4 col-lg-4">
+                    <div className="col-xs-6 col-sm-4 col-md-4 col-lg-4 col-xl-6">
                         <h5>Blagovest Damqnov</h5>
                     </div>
-                    <div className="col-xs-6 col-sm-4 col-md-4 col-lg-4">
+                    <div className="col-xs-6 col-sm-4 col-md-4 col-lg-4 col-xl-6">
                         <h5>Martin Yordanov</h5>
                     </div>
                 </div>
                 <div className="row">
-                    <div className="col-xs-6 col-sm-4 col-md-4 col-lg-4">
+                    <div className="col-xs-6 col-sm-4 col-md-4 col-lg-4 col-xl-6">
                         <h5>Martin Marinov</h5>
                     </div>
-                    <div className="col-xs-6 col-sm-4 col-md-4 col-lg-4">
+                    <div className="col-xs-6 col-sm-4 col-md-4 col-lg-4 col-xl-6">
                         <h5>Veli Aschikov</h5>
                     </div>
                 </div>
                 <div className="row">
-                    <div className="col-xs-6 col-sm-4 col-md-4 col-lg-4">
+                    <div className="col-xs-6 col-sm-4 col-md-4 col-lg-4 col-xl-6">
                         <h5>Kristiqn Manov</h5>
                     </div>
-                    <div className="col-xs-6 col-sm-4 col-md-4 col-lg-4">
+                    <div className="col-xs-6 col-sm-4 col-md-4 col-lg-4 col-xl-6">
                         <h5>Gabriel Yordanov</h5>
                     </div>
                 </div>
