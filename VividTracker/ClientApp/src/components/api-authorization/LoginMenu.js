551b1845ac1bd3512f1c9091b7eb5f1451bd36ce
BlagoDamqnov <chessplayer89@abv.bg>
2022-11-28 10:36:39 +0200

    Remove authView(logout button)

--- VividTracker/ClientApp/src/components/api-authorization/LoginMenu.js
+++ VividTracker/ClientApp/src/components/api-authorization/LoginMenu.js
@@ -46,12 +46,12 @@ export class LoginMenu extends Component {
 
     authenticatedView(userName, profilePath, logoutPath) {
         return (<Fragment>
-            <NavItem>
-                <NavLink tag={Link} className="text-dark" to={profilePath}>Hello {userName}</NavLink>
-            </NavItem>
-            <NavItem>
-                <NavLink tag={Link} className="text-dark" to={logoutPath}>Logout</NavLink>
-            </NavItem>
+            {/*<NavItem>*/}
+            {/*    <NavLink tag={Link} className="text-dark" to={profilePath}>Hello {userName}</NavLink>*/}
+            {/*</NavItem>*/}
+            {/*<NavItem>*/}
+            {/*    <NavLink tag={Link} className="text-dark" to={logoutPath}>Logout</NavLink>*/}
+            {/*</NavItem>*/}
         </Fragment>);
 
     }
