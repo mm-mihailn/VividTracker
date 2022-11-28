9ad3cb4126431c753f74da1c269fc218e61cda90
GoofyGalaxy374 <prophetyordanov@gmail.com>
2022-11-28 10:57:34 +0200

    Updated the interests section for Kristiyan Manov

--- VividTracker/ClientApp/src/components/LandingPage/LandingPage.js
+++ VividTracker/ClientApp/src/components/LandingPage/LandingPage.js
@@ -67,7 +67,7 @@ export class LandingPage extends Component {
                   <div className='developerInformation'>
                     <p className='dev-info-row devName'><strong>Name:</strong> Kristiyan Manov</p>
                     <p className='dev-info-row devRole'><strong>Role:</strong> Tester </p>
-                    <p className='dev-info-row devThing'><strong>Interests:</strong> I have only rocks, but when I throw them they start to roll.</p>
+                    <p className='dev-info-row devThing'><strong>Interests:</strong> I like playing computer games, riding a bike, listening to music.</p>
                   </div>
                 </div>
                 <div className='developerCard'>
