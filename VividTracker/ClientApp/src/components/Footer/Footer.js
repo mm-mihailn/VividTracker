e12ea8d012f94c6cd8d669877158fe85737becbb
BlagoDamqnov <chessplayer89@abv.bg>
2022-11-28 10:48:47 +0200

    New footer design

--- VividTracker/ClientApp/src/components/Footer/Footer.js
+++ VividTracker/ClientApp/src/components/Footer/Footer.js
@@ -7,34 +7,18 @@ export class Footer extends Component {
     render() {
         return (
             <footer id='footer' className='fixed-bottom'>
+                <h5 className= " d-flex justify-content-center">DEVELOPERS:</h5>
                 <div id="main-content">
                     <div className=" d-flex justify-content-center">
                         <div className="row d-flex justify-content-center">
-                            <div>
-                                <h5>Martin Yordanov</h5>
-                            </div>
-                            <div>
-                                <h5>Blagovest Damqnov</h5>
-                            </div>
-                        </div>
-                        <div className="row d-flex justify-content-center">
-                            <div>
-                                <h5>Martin Marinov</h5>
-                            </div>
-                            <div>
-                                <h5>Veli Aschikov</h5>
-                            </div>
-                        </div>
-                        <div className="row d-flex justify-content-center">
-                            <div>
-                                <h5>Kristiqn Manov</h5>
-                            </div>
-                            <div>
-                                <h5>Gabriel Yordanov</h5>
-                            </div>
+                            <h6>Blagovest Damyanov,Martin Yordanov,Veli Ashikov</h6>
                         </div>
                     </div>
-                   
+                    <div className=" d-flex justify-content-center">
+                        <div className="row d-flex justify-content-center">
+                            <h6>Martin Marinov,Gabriel Yordanov,Kristian Manov</h6>
+                        </div>
+                    </div>
                 </div>
               
             </footer>
