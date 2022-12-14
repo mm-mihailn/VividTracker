b540cb4b5512b44a0b79480392db47321331d7b6
Blueonn33 <91683317+Blueonn33@users.noreply.github.com>
2022-12-14 18:15:02 +0200

    Page refreshed when Add button is clicked

--- VividTracker/ClientApp/src/components/AddTenant/AddTenant.js
+++ VividTracker/ClientApp/src/components/AddTenant/AddTenant.js
@@ -53,6 +53,9 @@ export class AddTenant extends Component {
             })
         }
     }
+    refreshPage() {
+        window.location.reload(false);
+    }
     handleChange(event) {
         this.setState({ value: event.target.value });
     }
@@ -94,6 +97,7 @@ export class AddTenant extends Component {
                                                 onChange={(e) => this.setState({ 'value': e.target.value })}
                                                 style={{ borderBottomColor: this.state.textColor }}
                                                 className={this.state.valid == false ? "form-control name name-error" : "form-control name"} 
+                                                
                                             />
                                             <div className="modal-footer border-0">
                                                 <div id="error">
@@ -102,7 +106,9 @@ export class AddTenant extends Component {
                                                 <button type="reset" id="close" className="btn btn-link" data-bs-dismiss="modal"
                                                     onClick={() => this.clear()}>Clear
                                                 </button>
-                                                <button type="submit" id="submit" method="post" className="btn" name="addTenant"> Add </button>
+                                            <button type="submit" id="submit" method="post" className="btn" name="addTenant"
+                                                onClick={this.refreshPage}
+                                            >Add</button>
                                             </div>
                                         </form>
                                     </div>
