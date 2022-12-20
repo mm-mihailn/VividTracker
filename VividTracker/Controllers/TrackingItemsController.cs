b593c74993564c94f85df12c0890cfd6e8afae45
BlagoDamqnov <chessplayer89@abv.bg>
2022-12-20 16:40:17 +0200

    Created method which take the all Tracking Items in target tenant

---
+++ VividTracker/Controllers/TrackingItemsController.cs
@@ -0,0 +1,32 @@
+﻿namespace VividTracker.Controllers
+{
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using VividTracker.Business.Services.Interfaces;
+    using VividTracker.Data.Models;
+
+    [ApiController]
+    public class TrackingItemsController : ControllerBase
+    {
+        private readonly ITrackingItemsService _trackingItemsService;
+
+        public TrackingItemsController(ITrackingItemsService trackingItemsService)
+        {
+            _trackingItemsService = trackingItemsService;
+        }
+
+        [HttpGet]
+        [Route("api/trackingItems/{tenantId}")]
+        public async Task<IActionResult> GetAllTrackingItems([FromRoute] int tenantId)
+        {
+            var trackingItems = await _trackingItemsService.GetTrackingItemsByTenantId(tenantId);
+
+            if (!trackingItems.Any())
+            {
+                return BadRequest("No exist tracking items!");
+            }
+           return Ok(trackingItems);
+        }
+
+    }
+}
