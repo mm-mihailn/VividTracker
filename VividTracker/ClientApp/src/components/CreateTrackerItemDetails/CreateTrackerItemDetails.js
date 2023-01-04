e62d1e0b783b08419fa0ead35bd6efdc662087cc
GoofyGalaxy374 <prophetyordanov@gmail.com>
2023-01-04 17:45:17 +0200

    Added the HTML skeleton to the component as well as its styling.

--- VividTracker/ClientApp/src/components/CreateTrackerItemDetails/CreateTrackerItemDetails.js
+++ VividTracker/ClientApp/src/components/CreateTrackerItemDetails/CreateTrackerItemDetails.js
@@ -1,9 +1,37 @@
 import React, { Component } from 'react'
-
+import {faPenToSquare } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import './Styles/CreateTrackerItemDetails.css'
 export default class CreateTrackerItemDetails extends Component {
   render() {
     return (
-      <div>CreateTrackerItemDetails</div>
+        <div className = 'CreateTrackerItemDetailsWrapper d-flex justify-content-center align-items-center'>
+        <div className = 'CreateTrackerItemDetailsContainer'>
+            <div className = 'TrackerItemDetailsHeaderWrapper d-flex'>
+                {/* TODO: Populate the fields below wit the tracker data */}
+                <h4 className = 'TrackerItemDetailsName pageText'>Tracker Item Name</h4>
+                <FontAwesomeIcon className = 'CreateTrackerItemDetailsIcon' icon = {faPenToSquare}/>
+            </div>
+            <div className = 'TrackerItemDetailsFormWrapper'>
+                <div className = 'TrackerItemDetailsFormContainer'>
+                    <div className = 'TrackerItemDetailsFieldsContainer d-flex'>
+                        <label className = 'TrackerItemDetailsNameLabel pageText'>Tracker Item Name:</label>
+                        <input className = 'TrackerItemDetailsNameInputField form-control' type = 'text' value={''} onChange = {()=>{}}/>
+                    </div>
+                    <div className = 'TrackerItemDetailsFieldsContainer d-flex'>
+                    <label className = 'TrackerItemDetailsNameLabel pageText'>Tracker Item Type:</label>
+                        <input className = 'TrackerItemDetailsNameInputField form-control' type = 'text' value={''} onChange = {()=>{}}/>
+                    </div>
+                    <div className='TrackerItemDetailsButtons'>
+                        <span className='CancelButton'>Cancel</span>
+                        {/* Create function that will handle the request for the tracker item creation */}
+                        <button className='CreateButton' onClick={()=>{}}>Create</button>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+      </div>
     )
   }
 }
