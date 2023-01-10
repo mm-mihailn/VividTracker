bb884eb4137a8b33d7c8b4bdcb18297ea857caa4
Blueonn33 <91683317+Blueonn33@users.noreply.github.com>
2023-01-10 13:50:51 +0200

    the buttons of the tenant modal were displaced

--- VividTracker/ClientApp/src/components/AddTenant/AddTenant.js
+++ VividTracker/ClientApp/src/components/AddTenant/AddTenant.js
@@ -7,7 +7,7 @@ export class AddTenant extends Component {
         this.state = {
             value: '',
             errorMessage: '',
-            textColor: ''
+            textColor: '',
         }
         this.createTenant = this.createTenant.bind(this);
         this.handleChange = this.handleChange.bind(this);
@@ -43,17 +43,17 @@ export class AddTenant extends Component {
                 },
                 body: JSON.stringify({ "name": input })
             })
-            .then((response) => {
-                if (response.status == 400) {
-                    this.setState({ errorMessage: errors.existingTenant });
-                    this.setState({ textColor: color.error });
-                }
-                else {
-                    this.setState({ errorMessage: errors.success });
-                    this.setState({ textColor: color.success });
-                    this.props.onTenantAdded(this.props.value);
-                }
-            })
+                .then((response) => {
+                    if (response.status == 400) {
+                        this.setState({ errorMessage: errors.existingTenant });
+                        this.setState({ textColor: color.error });
+                    }
+                    else {
+                        this.setState({ errorMessage: errors.success });
+                        this.setState({ textColor: color.success });
+                        this.props.onTenantAdded(this.props.value);
+                    }
+                })
         }
     }
     handleChange(event) {
@@ -72,41 +72,41 @@ export class AddTenant extends Component {
             <div className="container">
                 <div className="container" id="modal">
                     <button type="button" id="createTenant" className="btn btn-link" data-bs-toggle="modal" data-bs-target="#addTenаntModal">
-                            CreateNewTenant()
+                        CreateNewTenant()
                     </button>
                 </div>
                 <div className="modal fade" id="addTenаntModal" role="dialog">
-                        <div className="modal-dialog">
-                            <div className="modal-content">
-                                <div className="modal-header border-0">
-                                    <button type="button" className="close" data-bs-dismiss="modal">&times;</button>
-                                    <div className="title">
-                                        <h4 className="modal-title" id="title">Add Tenant</h4>
-                                    </div>
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            <div className="modal-header border-0">
+                                <button type="button" className="close" data-bs-dismiss="modal">&times;</button>
+                                <div className="title">
+                                    <h4 className="modal-title" id="title">Add Tenant</h4>
                                 </div>
-                                <div className="modal-body">
-                                    <div id="myForm">
-                                        <form onSubmit={this.createTenant}>
-                                            <label htmlFor="tenantName" id="label-text">Tenant name:</label>
-                                            <input type="text" name="tenantName" className="form-control" id="name"
-                                                onChange={(e) => this.setState({ 'value': e.target.value })}
-                                                style={{ borderBottomColor: this.state.textColor }}
-                                                className={this.state.valid == false ? "form-control name name-error" : "form-control name"} 
-                                            />
-                                            <div className="modal-footer border-0">
-                                                <div id="error">
-                                                    <p style={{ color: this.state.textColor }}>{this.state.errorMessage}</p>
-                                                </div>
-                                                <button type="reset" id="close" className="btn btn-link" data-bs-dismiss="modal"
-                                                    onClick={() => this.clear()} >Clear
-                                                </button>
-                                            <button type="submit" id="submit" method="post" className="btn" name="addTenant">Add</button>
+                            </div>
+                            <div className="modal-body">
+                                <div id="myForm">
+                                    <form onSubmit={this.createTenant}>
+                                        <label htmlFor="tenantName" id="label-text">Tenant name:</label>
+                                        <input type="text" name="tenantName" className="form-control" id="name"
+                                            onChange={(e) => this.setState({ 'value': e.target.value })}
+                                            style={{ borderBottomColor: this.state.textColor }}
+                                            className={this.state.valid == false ? "form-control name name-error" : "form-control name"}
+                                        />
+                                        <div className="modal-footer border-0">
+                                            <div id="error">
+                                                <p style={{ color: this.state.textColor }}>{this.state.errorMessage}</p>
                                             </div>
-                                        </form>
-                                    </div>
+                                            <button type="reset" id="close" className="btn btn-link" data-bs-dismiss="modal"
+                                                onClick={() => this.clear()} >Clear
+                                            </button>
+                                            <button type="submit" id="submit" method="post" className="btn" name="addTenant">Add</button>
+                                        </div>
+                                    </form>
                                 </div>
                             </div>
                         </div>
+                    </div>
                 </div>
             </div>
         );
