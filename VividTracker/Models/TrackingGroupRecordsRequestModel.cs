1636fcb50400e47f43d3c6390caefc2e5b27f351
BlagoDamqnov <chessplayer89@abv.bg>
2023-02-01 16:29:00 +0200

    Added a functionality to create TrackingGroupRecords

---
+++ VividTracker/Models/TrackingGroupRecordsRequestModel.cs
@@ -0,0 +1,23 @@
+﻿namespace VividTracker.Models
+{
+    using VividTracker.Data.Models;
+
+    public class TrackingGroupRecordsRequestModel
+    {
+        public string Name { get; set; }
+        public int TrackingGroupId { get; set; }
+        public int? ParentId { get; set; }
+        public bool Disabled { get; set; }
+        public TrackingGroupRecord TrackingGroupRecords(TrackingGroup trackingGroup)
+        {
+            return new TrackingGroupRecord()
+            {
+                Name = Name,
+                TrackingGroupId = trackingGroup.Id,
+                Disabled= Disabled,
+                ParentId = ParentId,
+                TrackingGroup = trackingGroup
+            };
+        }
+    }
+}
