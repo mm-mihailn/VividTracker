64d50fe5bce028cb5f26dbbc1427c4b3e834de6c
BlagoDamqnov <chessplayer89@abv.bg>
2023-02-01 15:54:54 +0200

    Added a new migration. It contains two new columns about tracking item

---
+++ VividTracker.Data/Migrations/20230201135108_AddedNewColumnInTrackingItem.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VividTracker.Data.Migrations
+{
+    public partial class AddedNewColumnInTrackingItem : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "MaxValueType",
+                table: "TrackingItems",
+                type: "decimal(18,2)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "MinValueType",
+                table: "TrackingItems",
+                type: "decimal(18,2)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MaxValueType",
+                table: "TrackingItems");
+
+            migrationBuilder.DropColumn(
+                name: "MinValueType",
+                table: "TrackingItems");
+        }
+    }
+}
--- VividTracker.Data/Models/TrackingItem.cs
+++ VividTracker.Data/Models/TrackingItem.cs
@@ -16,7 +16,7 @@ namespace VividTracker.Data.Models
             TrackingGroups = new List<TrackingGroup>();
         }
 
-        public TrackingItem(int tenantId,string name,bool irrelevantAllowed, bool mandatoryComment, decimal? defaultValue, string maxValueColor, string minValueColor, string irrelevantColor, decimal target, PropertyType type) :this()
+        public TrackingItem(int tenantId,string name,bool irrelevantAllowed, bool mandatoryComment, decimal? defaultValue, string maxValueColor, string minValueColor, string irrelevantColor, decimal target, PropertyType type,decimal? minValueType,decimal? maxValueType) :this()
         {
             TenantId=tenantId;
             Name = name ?? throw new ArgumentNullException(nameof(name));
@@ -28,6 +28,8 @@ namespace VividTracker.Data.Models
             IrrelevantColor = irrelevantColor;
             Target = target;
             Type = type;
+            MinValueType = minValueType;
+            MaxValueType = maxValueType;
         }
         public int Id { get; set; }
         public int TenantId { get; set; }
@@ -46,6 +48,8 @@ namespace VividTracker.Data.Models
         public decimal Target { get; set; }
         public PropertyType Type { get; set; }
         public string Name { get; set; }
+        public decimal? MinValueType { get; set; }
+        public decimal? MaxValueType { get; set; }
         public ICollection<TrackingItemValue> TrackingItemsValues{ get; set; }
         public ICollection<TrackingItemUserGroupsVisibility> TrackingItemsUserGroupsVisibilities { get; set; }
         public ICollection<TrackingGroup> TrackingGroups { get; set; }
