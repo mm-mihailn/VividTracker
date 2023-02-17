11061267186501b68c6867fc0e70dc363d97bbe3
Blueonn33 <91683317+Blueonn33@users.noreply.github.com>
2023-02-17 21:06:39 +0200

    added some new things

--- VividTracker.Data/Repositories/UsersRepository.cs
+++ VividTracker.Data/Repositories/UsersRepository.cs
@@ -7,7 +7,6 @@
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
-    using System.Threading.Tasks;
     using VividTracker.Data.Models;
     using VividTracker.Data.Repositories.Interfaces;
 
