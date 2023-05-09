7969474963dc452fc038ab3412a95d343ae7c872
MartinYordanov374 <martinyordanov284@gmail.com>
2023-05-09 12:01:26 +0300

    Updated the styling of the comments, so that they reflect the design more accurately.

--- VividTracker/ClientApp/src/components/PanelContainer/PanelContainer.js
+++ VividTracker/ClientApp/src/components/PanelContainer/PanelContainer.js
@@ -7,28 +7,25 @@ export default class PanelContainer extends Component {
     render() {
         const date = new Date(this.props.trackingItemValueActivityData.timeStamp);
         const formattedDate = date.toLocaleDateString("en-GB");
+        let month = date.toString().split(' ')[1]
+        let day = date.toString().split(' ')[2]
         return (
-            <div className='panelContainerIndividual d-flex' key={this.props.trackingItemValueActivityData.id}>
-                <div className='panelContentContainer'>
-                    <div className='userNameWrapper ml-auto'>
-                        <span className='userNameText pageText'>
+                <div className='panelContainerIndividual' key={this.props.trackingItemValueActivityData.id}>
+                
+                    <div className='userNameWrapper'>
+                        <span className='userNameText commentUsername'>
                             {this.props.trackingItemValueActivityData.user.userName}
+                        </span>
+                        <span className='dateText commentDate'>
+                            {month + ' ' + day}
                         </span>
                     </div>
                     <div className='panelCommentWrapper'>
+                        <FontAwesomeIcon className='commentsIcon' icon={faComments} />
                         <span className='panelComment pageText'> {this.props.trackingItemValueActivityData.comment}
                         </span>
                     </div>
-                    <div className='managePanelIconWrapper ml-auto'>
-                        <FontAwesomeIcon className='commentsIcon' icon={faComments} />
-                    </div>
-                    <div className='dateWrapper ml-auto'>
-                        <span className='dateText pageText'>
-                            {formattedDate}
-                        </span>
-                    </div>
                 </div>
-            </div>
         )
     }
 }
